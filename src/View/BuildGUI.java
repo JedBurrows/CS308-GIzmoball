1bb402379ff4916a52eaf8007a8121c5461f7c71
Heather McLean <heather.mclean.2015@uni.strath.ac.uk>
2018-01-27 20:18:20 +0000

    Added sliders for friction and gravity

--- src/View/BuildGUI.java
+++ src/View/BuildGUI.java
@@ -8,14 +8,13 @@ import java.awt.event.ActionListener;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class BuildGUI implements IModeGUI,ActionListener {
-    private JButton  btnGizmo, btnBall, btnAbsorb, btnLFlipper, btnRFlipper, btnRotate,
-            btnDel, btnMove, btnClear, btnCon, btnDisc, btnKeyCon, btnKeyDisc, btnRunMode;
+    private JButton  btnGizmo, btnBall, btnClear, btnCon, btnDisc, btnKeyCon, btnKeyDisc, btnRunMode;
 
 
     public BuildGUI() {
 
         JFrame buildFrame = new JFrame("Build Mode!");
-        buildFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        buildFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JMenuBar menuBar = new JMenuBar();
         buildFrame.setJMenuBar(menuBar);
@@ -29,7 +28,8 @@ public class BuildGUI implements IModeGUI,ActionListener {
 
 
         JPanel panelBtn = new JPanel(new GridLayout(10, 2));
-        panelBtn.setSize(150,400);
+        panelBtn.setSize(150,300);
+        panelBtn.setLocation(0,500);
         JPanel gizmoBoard = new JPanel(new GridLayout(20, 20));
         gizmoBoard.setPreferredSize(new Dimension(400,400));
         gizmoBoard.setBackground(Color.blue);
@@ -49,23 +49,11 @@ public class BuildGUI implements IModeGUI,ActionListener {
         boxGizmo.addItem("Absorber");
 
         btnBall = new JButton("Add Ball");
+        btnBall.setPreferredSize(new Dimension(10, 10));
         btnBall.addActionListener(this);
         ////text field for ball////
-        /*btnAbsorb = new JButton("Add Absorber");
-        btnAbsorb.addActionListener(this);
-        btnLFlipper = new JButton("Add Left Flipper");
-        btnLFlipper.addActionListener(this);
-        btnRFlipper = new JButton("Add Right Flipper");
-        btnRFlipper.addActionListener(this);
-        btnRotate = new JButton("Rotate");
-        btnRotate.addActionListener(this);
-        btnDel = new JButton("Delete");
-        btnDel.addActionListener(this);
-        btnMove = new JButton("Move");
-        btnMove.addActionListener(this);*/
         btnClear = new JButton("Clear Board");
         btnClear.addActionListener(this);
-        ///Sliders for Friction and Gravity///
         btnCon = new JButton("Connect");
         btnCon.addActionListener(this);
         btnDisc = new JButton("Disconnect");
@@ -77,27 +65,54 @@ public class BuildGUI implements IModeGUI,ActionListener {
         btnRunMode = new JButton("Run");
         btnRunMode.addActionListener(this);
 
+        JSlider frictionSlider1 = new JSlider();
+        frictionSlider1.addChangeListener(new SliderChangeListener());
+        JLabel frictionLabel1 = new JLabel("Friction");
+        JPanel frictionPanel1 = new JPanel();
+        frictionPanel1.setLayout(new GridLayout(1,2));
+        frictionPanel1.add(frictionSlider1);
+        frictionPanel1.add(frictionLabel1,0);
+
+        JSlider frictionSlider2 = new JSlider();
+        frictionSlider2.addChangeListener(new SliderChangeListener());
+        JLabel frictionLabel2 = new JLabel("Friction");
+        JPanel frictionPanel2 = new JPanel();
+        frictionPanel2.setLayout(new GridLayout(1,2));
+        frictionPanel2.add(frictionSlider2);
+        frictionPanel2.add(frictionLabel2,0);
+
+        JSlider gravitySlider = new JSlider();
+        gravitySlider.addChangeListener(new SliderChangeListener());
+        JLabel gravityLabel = new JLabel("Gravity");
+        JPanel gravityPanel = new JPanel();
+        gravityPanel.setLayout(new GridLayout(1,2));
+        gravityPanel.add(gravitySlider);
+        gravityPanel.add(gravityLabel,0);
+
+        JPanel slidersPanel = new JPanel();
+        slidersPanel.setLayout(new GridLayout(3, 1));
+        slidersPanel.add(frictionPanel1);
+        slidersPanel.add(frictionPanel2);
+        slidersPanel.add(gravityPanel);
 
         panelBtn.add(btnGizmo);
         panelBtn.add(boxGizmo);
         panelBtn.add(btnBall);
-       /* panelBtn.add(btnAbsorb);
-        panelBtn.add(btnLFlipper);
-        panelBtn.add(btnRFlipper);
-        panelBtn.add(btnRotate);
-        panelBtn.add(btnDel);
-        panelBtn.add(btnMove);*/
         panelBtn.add(btnClear);
         panelBtn.add(btnCon);
         panelBtn.add(btnDisc);
         panelBtn.add(btnKeyCon);
         panelBtn.add(btnKeyDisc);
         panelBtn.add(btnRunMode);
+        panelBtn.add(frictionPanel1);
+        panelBtn.add(frictionPanel2);
+        panelBtn.add(gravityPanel);
 
+        buildFrame.add(menuBar);
+        
 
         buildFrame.add(menuBar);
         JPanel panel = new JPanel();
-        //panel.setPreferredSize(new Dimension(800, 800));
         panel.add(panelBtn);
         panel.add(gizmoBoard);
         buildFrame.add(panel);
@@ -106,6 +121,7 @@ public class BuildGUI implements IModeGUI,ActionListener {
         buildFrame.setLocationRelativeTo(null);
         buildFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
         buildFrame.setVisible(true);
+
 
     }
 
