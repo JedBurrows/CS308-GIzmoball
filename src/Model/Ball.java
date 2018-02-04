ff40fbb0ea0d589cf45e68db6ec087ef4357f90c
Sara Reid <sara.reid.2015@uni.strath.ac.uk>
2018-02-04 17:36:32 +0000

    looking at paint component

--- src/Model/Ball.java
+++ src/Model/Ball.java
@@ -1,4 +1,20 @@
 package Model;
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public class Ball {
+
+public class Ball extends JPanel {
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        g.drawOval(0,0,150,150);
+
+    }
+
 }
+
+
+
+
