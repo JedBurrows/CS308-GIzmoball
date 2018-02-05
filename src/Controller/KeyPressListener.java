e74bbcab7625850e5737cd0f4ddb29342d5ec7bc
Ciaran Service <ciaran.service.2015@uni.strath.ac.uk>
2018-02-05 16:24:03 +0000

    tried 1

---
+++ src/Controller/KeyPressListener.java
@@ -0,0 +1,24 @@
+package Controller;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyPressListener implements KeyListener {
+
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        System.out.println("hello");
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+}
+
